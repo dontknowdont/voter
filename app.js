22884037772bdd6549a4a509807220e4ccb54884
devR2001 <144663791+devR2001@users.noreply.github.com>
2023-10-21 18:03:50 +0200

    methods

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
-    //Optionen
-})
+  //Optionen
+  data: function () {
+    return {
+      submissions: submissions, // aus seed.js
+    };
+  },
+  methods: {
+    // upvote: function(){}
+    upvote() {
+        console.log(this.submissions[0].votes)
+        this.submissions[0].votes++
+    },
+  }
+});
 
 // # Repreäsentiert eine ID
-
 // Liefert eine Instanz zur Root-Component zurück
-const vm = app.mount('#app')
+const vm = app.mount("#app");
