6487679e949b1234bc716e031461221ef1d2c495
devR2001 <144663791+devR2001@users.noreply.github.com>
2023-10-23 20:00:30 +0200

    computed vs methods

--- app.js
+++ app.js
@@ -5,20 +5,32 @@ const app = Vue.createApp({
       submissions: submissions, // aus seed.js
     };
   },
+  computed: {
+    totalVotes() {
+    console.log('Computed Property ausgeführt');
+      return this.submissions.reduce((totalVotes, submission) => {
+        return totalVotes + submission.votes;
+      }, 0);
+    },
+  },
   methods: {
     // upvote: function(){}
     upvote(infoText) {
-        console.log(this.submissions[0].votes)
-        this.submissions[0].votes++
-        // console.log(infoText)
-        // console.log(event);
+      console.log(this.submissions[0].votes);
+      this.submissions[0].votes++;
+      // console.log(infoText)
+      // console.log(event);
     },
-    logConsole(text){
-        console.log(text);
-    }
-  }
+    logConsole(text) {
+      console.log(text);
+    },
+    // totalVotes(){
+    //  return this.submissions.reduce((totalVotes, submission) => {
+    //     return totalVotes + submission.votes
+    //   }, 0)
+    // }
+  },
 });
-
 
 // # Repreäsentiert eine ID
 // Liefert eine Instanz zur Root-Component zurück
