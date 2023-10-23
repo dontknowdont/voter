e81ec1c41840b30e9bb67f7b4251ec6908ffe686
devR2001 <144663791+devR2001@users.noreply.github.com>
2023-10-23 21:11:26 +0200

    notes

--- app.js
+++ app.js
@@ -36,6 +36,17 @@ const app = Vue.createApp({
     //   console.log('newValue');
     //   console.log('oldValue');
     // }
+    submissions: {
+      handler(newValue, oldValue){
+        // console.log('newValue', newValue);
+        // console.log('oldValue', oldValue);
+      this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
+        return totalVotes + submission.votes
+      },0)
+      },
+      deep: true,
+      immediate: true
+    },
     totalVotes(newValue, oldValue){
       console.log('newValue', newValue);
       console.log('oldValue', oldValue);
@@ -46,3 +57,18 @@ const app = Vue.createApp({
 // # Repreäsentiert eine ID
 // Liefert eine Instanz zur Root-Component zurück
 const vm = app.mount("#app");
+
+
+// Computed
+// Wenn abhängige Daten geändert werden
+// Daten, welche von anderen Daten abhängig sind
+
+// Mehtods
+// Wenn "sichtbare" Daten geändert werden, 
+// d.h. das, dass Template neu gerendert werden muss
+// Event-Listener ("clicks") oder für Daten die sich permanent ändern
+
+// Watcher
+// Wenn die beobachteten Daten sich ändern
+// Sollte Code ausführen welcher sich nicht direkt auf das Template auswirkt,
+// z.B. HTTP-Anfragen, den localStorage nutzen usw.
